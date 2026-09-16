6a640dd78ec87ea4a3e0ac3d7cb49d518336ad37
alovanecrypto <alon11osorio@gmail.com>
2026-09-15 18:56:02 -0600

    Update calificaciónapp.py

--- calificaciónapp.py
+++ calificaciónapp.py
@@ -153,5 +153,5 @@ def home():
     return render_template_string(HTML, promedio=promedio, nombre_completo=nombre_completo)
 
 if __name__ == '__main__':
-    webbrowser.open("http://127.0.0.1:5000")
-    app.run(port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
